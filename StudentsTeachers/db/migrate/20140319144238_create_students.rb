50f7ca84d2b525347a501e3f2360fee17235b4e3
Araphel <bartekus@gmail.com>
2015-07-09 12:55:45 -0700

    "Students integrated, imported and passing the test. "

--- StudentsTeachers/db/migrate/20140319144238_create_students.rb
+++ StudentsTeachers/db/migrate/20140319144238_create_students.rb
@@ -1,11 +1,18 @@
 class CreateStudents < ActiveRecord::Migration
   
-  def change
+  def chnage
     # Add code to create the table here
     # HINT: check out ActiveRecord::Migration.create_table
     create_table :students do |t|
       # column definitions go here
       # Use the AR migration guide for syntax reference
+      t.column      :first_name,  :string
+      t.column      :last_name,   :string
+      t.column      :gender,      :string
+      t.column      :email,       :string
+      t.column      :phone,       :string
+      t.column      :birthday,    :date
+      t.timestamps  null:         true
     end
   end
 
