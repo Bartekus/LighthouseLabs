fde81f1138458f70c4a20454be1b813f95d32ae6
Araphel <bartekus@gmail.com>
2015-07-11 23:02:48 -0700

    "Fourth Step Done"

--- FirstSinatraApp/app/models/message.rb
+++ FirstSinatraApp/app/models/message.rb
@@ -1,8 +1,3 @@
 class Message < ActiveRecord::Base
 
 end
-
-
-Message.create title: "First message", author: "Someone", content: "KV is so cool"
-Message.create title: "Second message", author: "Someone else", content: "So is fridge"
-Message.create title: "Third message", author: "Nobody", content: "..."
