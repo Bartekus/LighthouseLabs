fcd0e89e072a4dd94916a1eefdead43be3eb16f2
Araphel <bartekus@gmail.com>
2015-07-11 18:43:50 -0700

    "First Commit"

--- StudentsTeachers/db/schema.rb
+++ StudentsTeachers/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 3) do
 
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "students_teachers", id: false, force: :cascade do |t|
+    t.integer "student_id"
+    t.integer "teacher_id"
+  end
+
+  add_index "students_teachers", ["student_id"], name: "index_students_teachers_on_student_id"
+  add_index "students_teachers", ["teacher_id"], name: "index_students_teachers_on_teacher_id"
 
   create_table "teachers", force: :cascade do |t|
     t.string   "name"
