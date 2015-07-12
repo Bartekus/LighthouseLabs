85e3e35a751a8dde71251e42c4f444708362fb38
Araphel <bartekus@gmail.com>
2015-07-12 04:53:19 -0700

    "Seventh Exercise"

--- FirstSinatraApp/app/actions.rb
+++ FirstSinatraApp/app/actions.rb
@@ -7,3 +7,18 @@ get '/messages' do
   @messages = Message.all
   erb :'messages/index'
 end
+
+post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author:  params[:author]
+  )
+  @message.save
+  redirect '/messages'
+end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end
