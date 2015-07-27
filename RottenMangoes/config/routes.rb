c43ffd87cc4dbe2a2b85769dda74e9cffd589f47
Araphel <bartekus@gmail.com>
2015-07-26 17:44:52 -0700

    "Generating reviews controller and reviews/new view - Completed"

--- RottenMangoes/config/routes.rb
+++ RottenMangoes/config/routes.rb
@@ -1,13 +1,13 @@
 RottenMangoes::Application.routes.draw do
-  get 'sessions/new'
 
-  get 'sessions/create'
+  get 'reviews/new'
 
-  get 'users/new'
+  get 'reviews/create'
 
-  get 'users/create'
-
-  resources :movies
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+
 end
